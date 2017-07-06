7034383f34e0e9fbe56639a5a83646ee47d684e4
yoonkh <zizou0812@gmail.com>
2017-07-06 18:21:11 +0900

    member app setting

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, logout as django_logout, login as django_login
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user is not None:
+            django_login(request, user)
+            return redirect('post:post_list')
+        else:
+            return HttpResponse('Login credentials invalid')
+
+
+    else:
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
+        return render(request, 'member/login.html')
+
+
+def logout(request):
+    django_logout(request)
